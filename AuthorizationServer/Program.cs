184ba4d12ddc003876cd676a7446726978c04ab2
Mike Korakakis <mike.korakakis@gmail.com>
2021-11-21 22:05:41 +0200

    upgraded to dotnet 6

--- AuthorizationServer/Program.cs
+++ AuthorizationServer/Program.cs
@@ -2,25 +2,96 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthorizationServer;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AuthorizationServer
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    options.LoginPath = "/account/login";
                 });
-    }
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    // Configure the context to use an in-memory store.
+    options.UseInMemoryDatabase(nameof(DbContext));
+
+    // Register the entity sets needed by OpenIddict.
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the EF Core stores/models.
+        options.UseEntityFrameworkCore()
+.UseDbContext<DbContext>();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+        options
+            .AllowClientCredentialsFlow()
+            .AllowAuthorizationCodeFlow()
+                .RequireProofKeyForCodeExchange()
+            .AllowRefreshTokenFlow();
+
+        options
+            .SetTokenEndpointUris("/connect/token")
+            .SetAuthorizationEndpointUris("/connect/authorize")
+            .SetUserinfoEndpointUris("/connect/userinfo");
+
+        // Encryption and signing of tokens
+        options
+.AddEphemeralEncryptionKey()
+.AddEphemeralSigningKey()
+.DisableAccessTokenEncryption();
+
+        // Register scopes (permissions)
+        options.RegisterScopes("api");
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options
+.UseAspNetCore()
+.EnableTokenEndpointPassthrough()
+.EnableAuthorizationEndpointPassthrough()
+.EnableUserinfoEndpointPassthrough();
+    });
+
+builder.Services.AddHostedService<TestData>();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+await app.RunAsync();
